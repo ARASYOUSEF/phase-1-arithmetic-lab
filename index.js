395431b26841df2bd575147ba0f83d1d0da3d34f
ARASYOUSEF <aras.yousef.a@gmail.com>
2024-02-21 19:24:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
+let random = Math.floor(Math.random() * 100) + 1
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+
+
+
+
+
+
